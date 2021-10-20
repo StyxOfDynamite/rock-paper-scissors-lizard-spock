cabeb488f31402dd30a0617f4350b91bea9dc555
Luke Storer <lukes@macbookair>
2021-10-20 16:02:31 +0100

    feature: implements logging factory.

--- public/index.php
+++ public/index.php
@@ -5,25 +5,41 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\Computer;
 use App\Game;
 use App\Human;
+use App\LoggerFactory;
+use App\ScreenLogger;
+use App\SimpleFileLogger;
 
 $options = [
     'gameWonAt' => 5,
 ];
 
-$game = new Game($options);
-$player = new Human('John Doe');
-$computer = new Computer();
+$loggerFactory = new LoggerFactory;
+
+$loggerFactory->addProvider('simple', function () {
+    $instance = new SimpleFileLogger('log.txt');
+    return $instance;
+});
+
+$loggerFactory->addProvider('screen', function () {
+    $instance = new ScreenLogger();
+    return $instance;
+});
+
+
+
+
+
+$game = new Game($options, $loggerFactory);
+$player = new Human('John Doe', $loggerFactory);
+$computer = new Computer($loggerFactory);
 
 $game->addPlayer($player);
 $game->addPlayer($computer);
 
 while (! $game->isFinished()) {
-    $player->setMove($player->chooseMove());
-    printf("%s played %s\n", $player, $player->getMove());
-    
-    $computer->setMove($computer->chooseMove());
-    printf("%s played %s\n", $computer, $computer->getMove());
 
+    $player->setMove($player->chooseMove());
+    $computer->setMove($computer->chooseMove());
 
     if ($player->getMove()->draws($computer->getMove())) {
         printf("Draw\n");
